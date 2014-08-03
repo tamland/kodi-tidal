d559235c7f2e7fcdfe58cb2772faa4786dee6569
Thomas Amland <thomas.amland@gmail.com>
2014-08-03 12:47:41 +0200

    update wimpy

--- lib/wimpy/models.py
+++ lib/wimpy/models.py
@@ -3,35 +3,33 @@
 # Copyright (C) 2014 Thomas Amland
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 from __future__ import unicode_literals
 
 IMG_URL = "http://images.osl.wimpmusic.com/im/im?w={width}&h={height}&{id_type}={id}"
 
-class Immutable(object):
+
+class Model(object):
     id = None
     name = None
 
     def __init__(self, **kwargs):
-        for name, value in kwargs.items():
-            self.__dict__[name] = value
-
-    def __setattr__(self, name, value):
-        raise AttributeError('immutable')
+        self.__dict__.update(kwargs)
 
 
-class Album(Immutable):
+class Album(Model):
 
     artist = None
     num_tracks = -1
@@ -42,14 +40,14 @@ class Album(Immutable):
         return IMG_URL.format(width=width, height=height, id=self.id, id_type='albumid')
 
 
-class Artist(Immutable):
+class Artist(Model):
 
     @property
     def image(self, width=512, height=512):
         return IMG_URL.format(width=width, height=height, id=self.id, id_type='artistid')
 
 
-class Playlist(Immutable):
+class Playlist(Model):
     description = None
     creator = None
     type = None
@@ -65,7 +63,7 @@ class Playlist(Immutable):
 
 
 
-class Track(Immutable):
+class Track(Model):
     duration = -1
     track_num = -1
     popularity = -1
@@ -100,7 +98,7 @@ class User(object):
         return self._session.get_favorite_tracks(self.id)
 
 
-class SearchResult(Immutable):
+class SearchResult(Model):
     artists = []
     albums = []
     tracks = []
